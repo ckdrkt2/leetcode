41c040e1a785e2f1a4ce461cfa86f34bf0a893b4
ckdrkt2 <bbiuyy@gmail.com>
2021-08-02 23:24:00 +0900

    2021-08-02

--- 0237. Delete Node in a Linked List.py
+++ 0237. Delete Node in a Linked List.py
@@ -10,8 +10,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        while node.next:
-            node.val = node.next.val
-            prev = node
-            node = node.next
-        prev.next = None
+        node.val = node.next.val
+        node.next = node.next.next
+        

f02f97fa63556b083f69ba961816870f9ee783d5
ckdrkt2 <72551588+ckdrkt2@users.noreply.github.com>
2021-11-08 16:15:17 +0900

    2021-11-08

--- 0977.Squares_of_a_Sorted Array.py
+++ 0977.Squares_of_a_Sorted Array.py
@@ -1,5 +1,5 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
-            nums[i] *= nums[i]
+            nums[i] *= nums[i]j 
         return sorted(nums)

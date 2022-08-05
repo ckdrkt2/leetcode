9fa64c4766fd3a4b8071d4884aac97b232e0e277
ckdrkt2 <bbiuyy@gmail.com>
2022-08-05 14:08:21 +0000

    2022-08-05

---
+++ 0377. Combination Sum IV.py
@@ -0,0 +1,7 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [1] + [0] * target
+        for t in range(1, target + 1):
+            for n in nums:
+                if t - n >= 0: dp[t] += dp[t-n]
+        return dp[-1]

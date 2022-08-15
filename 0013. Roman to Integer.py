6f56835d697563078fafc16d43aba5668aa075d2
ckdrkt2 <bbiuyy@gmail.com>
2022-08-15 13:21:49 +0000

    2022-08-15

--- 0013. Roman to Integer.py
+++ 0013. Roman to Integer.py
@@ -1,24 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        a = list(s[::-1]); n = 0; t = 0
-        for i in range(len(a)):
-            if a[i] == 'I':
-                a[i] = 1
-            elif a[i] == 'V':
-                a[i] = 5
-            elif a[i] == 'X':
-                a[i] = 10
-            elif a[i] == 'L':
-                a[i] = 50
-            elif a[i] == 'C':
-                a[i] = 100
-            elif a[i] == 'D':
-                a[i] = 500
-            elif a[i] == 'M':
-                a[i] = 1000
-            if a[i] >= n:
-                n = a[i]
-                t += a[i]
-            else:
-                t -= a[i]
-        return t
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number

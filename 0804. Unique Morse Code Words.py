9b61683e9661321ddd06158d46cc069fa3606a7d
ckdrkt2 <bbiuyy@gmail.com>
2022-08-17 11:27:03 +0000

    2022-08-17

---
+++ 0804. Unique Morse Code Words.py
@@ -0,0 +1,4 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        MORSE = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..'}
+        return len({"".join(MORSE[c] for c in word) for word in words})

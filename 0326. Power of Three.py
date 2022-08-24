8aba98e87ee1b18f4130abf24ece52c9d38455d8
ckdrkt2 <bbiuyy@gmail.com>
2022-08-24 11:02:57 +0000

    2022-08-24

--- 0326. Power of Three.py
+++ 0326. Power of Three.py
@@ -1,10 +1,3 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n < 1 or n == 2:
-            return False
-        elif n == 1: return True
-        while n > 1 :
-            if n % 3 != 0:
-                return False
-            n = n // 3
-        return True
+        return n > 0 and 3 ** 19 % n == 0

0c87f38884eb715fe96ad29a6c11abdbd8a685ba
ckdrkt2 <bbiuyy@gmail.com>
2022-09-08 07:31:03 +0000

    2022-09-08

--- 0094.Binary_Tree_Inorder_Traversal.py
+++ 0094.Binary_Tree_Inorder_Traversal.py
@@ -3,9 +3,8 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right 
+#         self.right = right
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        if root == None:
-            return []
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None: return []
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)

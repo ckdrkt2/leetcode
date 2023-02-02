b23898f8bbfc691422ac875ccf06d2bdb657041c
ckdrkt2 <bbiuyy@gmail.com>
2023-02-02 23:53:55 +0900

    2023-02-02

---
+++ 0953. Verifying an Alien Dictionary.py
@@ -0,0 +1,4 @@
+from typing import List
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        return words == sorted(words, key=lambda x : [order.index(w) for w in x])

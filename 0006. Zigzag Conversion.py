a5b34cefc1a78aa803ec2a02fe8e24531be0652e
ckdrkt2 <bbiuyy@gmail.com>
2023-02-03 15:44:38 +0900

    2023-02-03

---
+++ 0006. Zigzag Conversion.py
@@ -0,0 +1,11 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s): return s
+        ans = [''] * numRows
+        index, step = 0, 1
+        for c in s:
+            ans[index] += c
+            if index == 0: step = 1
+            elif index == numRows - 1: step = -1
+            index += step
+        return ''.join(ans)

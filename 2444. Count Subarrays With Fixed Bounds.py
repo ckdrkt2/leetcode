ec1fcded32613b8812d879fab33322f801116229
ckdrkt2 <bbiuyy@gmail.com>
2023-03-04 17:58:11 +0900

    2023-03-04

---
+++ 2444. Count Subarrays With Fixed Bounds.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        ans = 0
+        isMin = isMax = isnot = -1
+        for i, num in enumerate(nums):
+            if not minK <= num <= maxK: isnot = i
+            if num == minK: isMin = i
+            if num == maxK: isMax = i
+            ans += max(0, min(isMin, isMax) - isnot)
+        return ans

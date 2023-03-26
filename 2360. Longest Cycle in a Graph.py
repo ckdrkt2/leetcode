9092369050392867a8d92f03759aa253da2ea5df
ckdrkt2 <bbiuyy@gmail.com>
2023-03-26 23:53:01 +0900

    2023-03-26

---
+++ 2360. Longest Cycle in a Graph.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        time = [0] * len(edges)
+        clock, ans = 1, -1
+        for x, t in enumerate(time):
+            if t:
+                continue
+            start_time = clock
+            while x >= 0:
+                if time[x]:
+                    if time[x] >= start_time:
+                        ans = max(ans, clock - time[x])
+                    break
+                time[x] = clock
+                clock += 1
+                x = edges[x]
+        return ans

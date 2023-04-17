f865ab420ae9d81013a24d0c14e2ea998ad37fed
ckdrkt2 <bbiuyy@gmail.com>
2023-04-17 23:09:32 +0900

    2023-04-17

---
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,4 @@
+from typing import List
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [i >= max(candies) - extraCandies for i in candies]

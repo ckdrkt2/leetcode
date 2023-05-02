d3c930e9998f0574800ff1c50cd27466efd5103b
ckdrkt2 <bbiuyy@gmail.com>
2023-05-02 21:41:30 +0700

    2023-05-02

--- 1822. Sign of the Product of an Array.py
+++ 1822. Sign of the Product of an Array.py
@@ -1,6 +1,5 @@
-```python
+from typing import List
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
         a = eval('(' + ')*('.join(map(str,nums)) + ')')
         return 1 if a > 0 else -1 if a < 0 else 0
-```

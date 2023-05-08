3b73627fb1162420e8ef2b2b3a7cb018610bb997
ckdrkt2 <bbiuyy@gmail.com>
2023-05-08 14:20:52 +0900

    2023-05-08

---
+++ 1572. Matrix Diagonal Sum.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ans = 0
+        n = len(mat)
+        for i, row in enumerate(mat):
+            j = n - i - 1
+            ans += row[i] + (0 if j == i else row[j])
+        return ans

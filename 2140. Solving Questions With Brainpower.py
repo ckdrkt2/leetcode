d118e35ef1b15935b6f5749937d72d63475f7d83
ckdrkt2 <bbiuyy@gmail.com>
2023-05-12 23:09:32 +0900

    2023-05-12

---
+++ 2140. Solving Questions With Brainpower.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            points, jump = questions[i][0], questions[i][1]
+            dp[i] = max(points + dp[min(jump + i + 1, n)], dp[i + 1])
+        return dp[0]

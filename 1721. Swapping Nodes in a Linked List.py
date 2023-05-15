671aa55f5fb981f42d9bcef9f2a44b23d7102986
ckdrkt2 <bbiuyy@gmail.com>
2023-05-15 15:51:42 +0900

    2023-05-15

---
+++ 1721. Swapping Nodes in a Linked List.py
@@ -0,0 +1,16 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        slow, fast = head, head
+        for _ in range(k - 1):
+            fast = fast.next
+        first = fast
+        while fast.next:
+            slow, fast = slow.next, fast.next
+        first.val, slow.val = slow.val, first.val
+        return head

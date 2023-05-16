88d58989534d369350bc568be7e17786b3a7d4a3
ckdrkt2 <bbiuyy@gmail.com>
2023-05-16 12:39:40 +0900

    2023-05-16

--- 0024. Swap Nodes in Pairs.py
+++ 0024. Swap Nodes in Pairs.py
@@ -1,9 +1,9 @@
-```python
+from typing import Optional
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:          
         if head is None or head.next is None:
@@ -13,7 +13,4 @@ class Solution:
         head, head.next=head.next, head
         head.next.next=self.swapPairs(node)
         return head
-```
 
-time complexity: O(N)
-space complexity: O(N)

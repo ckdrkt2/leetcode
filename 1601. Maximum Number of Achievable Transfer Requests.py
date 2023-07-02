1cd35303540cfc6ef5ff37379d3016bc9f3f0757
ckdrkt2 <bbiuyy@gmail.com>
2023-07-02 14:35:07 +0900

    2023-07-02

---
+++ 1601. Maximum Number of Achievable Transfer Requests.py
@@ -0,0 +1,12 @@
+from typing import List
+from itertools import combinations
+class Solution:
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        for k in range(len(requests), 0, -1):
+            for c in combinations(range(len(requests)), k):
+                degree = [0] * n
+                for i in c:
+                    degree[requests[i][0]] -= 1
+                    degree[requests[i][1]] += 1
+                if not any(degree): return k
+        return 0

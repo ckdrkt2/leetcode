516906b07640272170b517cb770cba1643cd2573
ckdrkt2 <bbiuyy@gmail.com>
2023-07-30 20:42:55 +0900

    2023-07-30

---
+++ 0664. Strange Printer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        n = len(s)
+        dp = [[101] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = 1
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i][j - 1]
+                else:
+                    for k in range(i, j):
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j])
+
+        return dp[0][n - 1]

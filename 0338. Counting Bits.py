169fbf2afa583f2915eb55e1e5189d01c8b40efb
ckdrkt2 <bbiuyy@gmail.com>
2023-09-01 23:55:32 +0900

    2023-09-01

--- 0338. Counting Bits.py
+++ 0338. Counting Bits.py
@@ -1,10 +1,11 @@
+from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        a = []
-        for i in range(n+1):
-            num = i; c = 0
+        ans = []
+        for i in range(n + 1):
+            num, c = i, 0
             while num >= 1:
-                c += num%2
-                num = num//2
-            a.append(c)
-        return a
+                c += num % 2
+                num = num // 2
+            ans.append(c)
+        return ans

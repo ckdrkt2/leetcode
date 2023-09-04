3fe1ce67d1cb8307f6221ae58b9b913c39e93fa4
ckdrkt2 <bbiuyy@gmail.com>
2023-09-04 18:02:41 +0900

    2023-09-04

--- 0141. Linked List Cycle.py
+++ 0141. Linked List Cycle.py
@@ -1,19 +1,14 @@
-```python
+from typing import Optional
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         a, b = head, head
         while b and b.next:
             a = a.next
             b = b.next.next
-            if a == b:
-                return True
+            if a == b: return True
         return False
-```
-
-time complexity: O(N)   
-space complexity: O(1)

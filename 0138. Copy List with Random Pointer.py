341419a5d9a8ed190d728b6fc53c07aef3eab442
ckdrkt2 <bbiuyy@gmail.com>
2023-09-05 18:16:24 +0900

    2023-09-05

--- 0138. Copy List with Random Pointer.py
+++ 0138. Copy List with Random Pointer.py
@@ -1,17 +1,15 @@
-```python
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None: return None
-        a = [Node(0)]
-        b = [head]
+        a, b = [Node(0)], [head]
         while True:
             a[-1].val = b[-1].val
             if b[-1].next is None: break
@@ -21,7 +19,3 @@ class Solution:
         for i, node in enumerate(b):
             a[i].random = None if node.random is None else a[b.index(node.random)]
         return a[0]
-```
-
-time complexity: O(N)       
-space complexity: O(N)

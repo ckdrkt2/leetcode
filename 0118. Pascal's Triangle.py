32c8ed4997e942bd8e1ced6f25ef899b3295ef93
ckdrkt2 <bbiuyy@gmail.com>
2023-09-08 23:55:32 +0900

    2023-09-08

--- 0118. Pascal's Triangle.py
+++ 0118. Pascal's Triangle.py
@@ -1,13 +1,13 @@
+from typing import List
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ex = [1]; out = []; now = []
+        ex, now, ans = [1], [], []
         for i in range(numRows):
             for j in range(i+1):
                 if j == i or j == 0:
                     now.append(1)
                 else:
                     now.append(ex[j-1] + ex[j])
-            out.append(now)
-            ex = [n for n in now]
-            now = []
-        return out
+            ans.append(now)
+            ex, now = [n for n in now], []
+        return ans

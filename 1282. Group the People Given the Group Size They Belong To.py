fc53b550c9f22335e2daf32dc83edb122653897a
ckdrkt2 <bbiuyy@gmail.com>
2023-09-11 23:57:01 +0900

    2023-09-11

--- 1282. Group the People Given the Group Size They Belong To.py
+++ 1282. Group the People Given the Group Size They Belong To.py
@@ -1,18 +1,11 @@
-```python
+from typing import List
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        a = {}; b = []
+        a, b = {}, []
         for i, n in enumerate(groupSizes):
-            if n not in a:
-                a[n] = [i]
-            else:
-                a[n].append(i)
+            if n not in a: a[n] = [i]
+            else: a[n].append(i)
         for i in sorted(a):
             for j in range(0, len(a[i]), i):
                 b.append(a[i][j:j+i])
         return b
-```
-
-space complexity: O(N)
-
-time complexity: O(N)

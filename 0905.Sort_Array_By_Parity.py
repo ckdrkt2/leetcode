1c24a1b036b17af2719f04fbc72f492023497d6c
ckdrkt2 <bbiuyy@gmail.com>
2023-09-28 23:32:39 +0900

    2023-09-28

--- 0905.Sort_Array_By_Parity.py
+++ 0905.Sort_Array_By_Parity.py
@@ -1,9 +1,10 @@
+from typing import List
 class Solution:
-    def sortArrayByParity(self, A: List[int]) -> List[int]:
-        for i in range(len(A)):
-            if A[i] % 2 != 0:
-                A.append(A[i])
-                A[i] = -1
-        for i in range(A.count(-1)):
-            A.remove(-1)
-        return A
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i] % 2 != 0:
+                nums.append(nums[i])
+                nums[i] = -1
+        for _ in range(nums.count(-1)):
+            nums.remove(-1)
+        return nums

05fac261a2a5f96c650be3033321d5f1b5641f5a
ckdrkt2 <bbiuyy@gmail.com>
2023-09-29 23:37:07 +0900

    2023-09-29

---
+++ 0896. Monotonic Array.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        pattern = nums[len(nums) - 1] > nums[0]
+        return all((nums[i] > nums[i-1]) == pattern or nums[i] == nums[i-1] for i in range(1, len(nums)))

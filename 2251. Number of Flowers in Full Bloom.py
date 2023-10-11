c9b339d5086bc065dd18308817019442d37cc2ed
ckdrkt2 <bbiuyy@gmail.com>
2023-10-11 23:57:01 +0900

    2023-10-11

---
+++ 2251. Number of Flowers in Full Bloom.py
@@ -0,0 +1,20 @@
+from typing import List
+from heapq import heappush, heappop
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        flowers.sort()
+        sorted_people = sorted(people)
+        dic, heap = {}, []
+
+        i = 0
+        for person in sorted_people:
+            while i < len(flowers) and flowers[i][0] <= person:
+                heappush(heap, flowers[i][1])
+                i += 1
+
+            while heap and heap[0] < person:
+                heappop(heap)
+
+            dic[person] = len(heap)
+
+        return [dic[x] for x in people]

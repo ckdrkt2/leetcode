ddec6db4a07c96d701fce3860daa8618b667f2c5
ckdrkt2 <bbiuyy@gmail.com>
2023-10-21 23:57:01 +0900

    2023-10-21

---
+++ 1425. Constrained Subsequence Sum.py
@@ -0,0 +1,16 @@
+from typing import List
+from heapq import heappush, heappop
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        heap = [(-nums[0], 0)]
+        ans = nums[0]
+
+        for i in range(1, len(nums)):
+            while i - heap[0][1] > k:
+                heappop(heap)
+
+            curr = max(0, -heap[0][0]) + nums[i]
+            ans = max(ans, curr)
+            heappush(heap, (-curr, i))
+
+        return ans

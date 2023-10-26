7457448bb646d540dafde2165c2791f083427e0d
ckdrkt2 <bbiuyy@gmail.com>
2023-10-26 23:50:01 +0900

    2023-10-26

--- 0823. Binary Trees With Factors.py
+++ 0823. Binary Trees With Factors.py
@@ -1,12 +1,14 @@
+from typing import List
+from collections import defaultdict
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         arr.sort()
-        d = {_ : 0 for _ in arr}
+        d = defaultdict(int)
         for i, n in enumerate(arr):
             c = 1
             for x in arr[:i]:
                 if n % x: continue
                 y = n // x
-                c += d.get(x,0) * d.get(y,0)
+                c += d.get(x, 0) * d.get(y, 0)
             d[n] = c
         return sum(d.values()) % 1000000007

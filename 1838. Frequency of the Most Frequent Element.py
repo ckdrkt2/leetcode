4cb6957fc1e9399186df9aec2c117f96e688efb4
ckdrkt2 <bbiuyy@gmail.com>
2023-11-18 23:09:32 +0900

    2023-11-18

---
+++ 1838. Frequency of the Most Frequent Element.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        left = right = 0
+        nums.sort()
+        for right in range(len(nums)):
+            k += nums[right]
+            if k < nums[right] * (right - left + 1):
+                k -= nums[left]
+                left += 1
+        return right - left + 1

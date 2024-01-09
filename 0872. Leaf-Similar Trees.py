98977584642648a230bd0d3e1e5d0d821761e726
ckdrkt2 <bbiuyy@gmail.com>
2024-01-09 20:48:34 +0900

    2024-01-09

--- 0872. Leaf-Similar Trees.py
+++ 0872. Leaf-Similar Trees.py
@@ -1,13 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if not root: return []
-            if not root.left and not root.right: return [root.val]
-            return dfs(root.left) + dfs(root.right)
-        return dfs(root1) == dfs(root2)
+        return self.dfs(root1) == self.dfs(root2)
+
+    def dfs(self, node):
+        if not node: return []
+        if node.left is None and node.right is None:
+            return [node.val]
+        return self.dfs(node.left) + self.dfs(node.right)

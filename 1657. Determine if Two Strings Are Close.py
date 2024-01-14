c6e56cd4739f2088e5fc78dfa87420580ffae965
ckdrkt2 <bbiuyy@gmail.com>
2024-01-14 15:57:30 +0900

    2024-01-14

--- 1657. Determine if Two Strings Are Close.py
+++ 1657. Determine if Two Strings Are Close.py
@@ -1,7 +1,6 @@
 from collections import Counter
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         c1, c2 = Counter(word1), Counter(word2)
-        f1, f2 = sorted(c1.values()), sorted(c2.values())
-        if f1 == f2 and sorted(c2.keys()) == sorted(c1.keys()): return True
-        return False
+        return c1.keys() == c2.keys() and sorted(c1.values()) == sorted(c2.values())

5609f20dfaebfff53481dd97511cc559ca0a32b1
ckdrkt2 <bbiuyy@gmail.com>
2024-01-17 21:37:32 +0900

    2024-01-17

--- 1207. Unique Number of Occurrences.py
+++ 1207. Unique Number of Occurrences.py
@@ -1,4 +1,7 @@
+from typing import List
 from collections import Counter
+
+
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         return len(set(arr)) == len(set(Counter(arr).values()))

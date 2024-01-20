290355efca39d6bed1bab4752e7f1067b765d1d1
ckdrkt2 <bbiuyy@gmail.com>
2024-01-20 21:37:32 +0900

    2024-01-20

--- 0907. Sum of Subarray Minimums.py
+++ 0907. Sum of Subarray Minimums.py
@@ -1,10 +1,12 @@
+from typing import List
+
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         arr = [0] + arr
-        result, stack = [0] * len(arr), [0]
+        ans, stack = [0] * len(arr), [0]
         for i in range(len(arr)):
             while arr[stack[-1]] > arr[i]: stack.pop()
             j = stack[-1]
-            result[i] = result[j] + (i - j) * arr[i]
+            ans[i] = ans[j] + (i - j) * arr[i]
             stack.append(i)
-        return sum(result) % 1000000007
+        return sum(ans) % 1000000007

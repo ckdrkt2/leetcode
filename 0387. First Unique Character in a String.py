49a2a74d7f7af9e289273aab995c39cad34dd2ca
ckdrkt2 <bbiuyy@gmail.com>
2024-02-05 21:37:32 +0900

    2024-02-05

--- 0387. First Unique Character in a String.py
+++ 0387. First Unique Character in a String.py
@@ -1,9 +1,11 @@
 from string import ascii_lowercase
 
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        a = 1e5
+        ans = 1e5
         for c in ascii_lowercase:
             l, r = s.find(c), s.rfind(c)
-            if l == r != -1: a = min(a, l)
-        return a if a < 1e5 else -1
+            if l == r != -1:
+                ans = min(ans, l)
+        return ans if ans < 1e5 else -1

3894900acbc506480064abca17121a4788e808b7
ckdrkt2 <bbiuyy@gmail.com>
2024-02-08 21:37:32 +0900

    2024-02-08

--- 0279. Perfect Squares.py
+++ 0279. Perfect Squares.py
@@ -1,14 +1,18 @@
 from collections import deque
+
+
 class Solution:
     def numSquares(self, n: int) -> int:
-        square = [(i+1)**2 for i in range(int(n**0.5))]
+        square = [(i + 1) ** 2 for i in range(int(n ** 0.5))]
         ans = float('inf')
-        q = deque([(n,0)])
+        q = deque([(n, 0)])
         while q:
             num, cnt = q.popleft()
             if cnt > ans: continue
             for s in square:
                 if s > num: break
-                if num % s == 0: ans = min(ans, cnt + (num // s))
-                else: q.append((num % s, cnt + (num // s)))
+                if num % s == 0:
+                    ans = min(ans, cnt + (num // s))
+                else:
+                    q.append((num % s, cnt + (num // s)))
         return ans

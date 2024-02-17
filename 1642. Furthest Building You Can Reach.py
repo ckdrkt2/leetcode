4083484565a08a784986d4127cc3c534c33dafa6
ckdrkt2 <bbiuyy@gmail.com>
2024-02-17 21:37:32 +0900

    2024-02-17

--- 1642. Furthest Building You Can Reach.py
+++ 1642. Furthest Building You Can Reach.py
@@ -1,22 +1,20 @@
-```python
+from typing import List
+from heapq import heappush, heappop
+
+
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        heap = []
-        n = len(heights)
+        heap, n = [], len(heights)
+
         for i in range(n-1):
             h = heights[i+1] - heights[i]
             if h <= 0: continue
-            
-            heappush(heap,h)
+
+            heappush(heap, h)
             if len(heap) > ladders:
                 min_height = heappop(heap)
-                bricks -= min_height 
+                bricks -= min_height
 
-            if bricks < 0:
-                return i
+            if bricks < 0: return i
 
-        return n-1
-```
-
-time complexity: O(N)   
-space complexity: O(N)
+        return n - 1

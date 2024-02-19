0b2b92b2fbc54e6499c92f13dd374717b37c117c
ckdrkt2 <bbiuyy@gmail.com>
2024-02-19 19:53:19 +0900

    2024-02-19

--- 0231. Power of Two.py
+++ 0231. Power of Two.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n < 1: return False
-        while n % 2 == 0:
-            n /= 2
+        while n % 2 == 0: n >>= 1
         return n == 1
+    

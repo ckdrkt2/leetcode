59648264a52112f3ebb1fc97e9f458f50ef91943
ckdrkt2 <bbiuyy@gmail.com>
2024-02-20 21:16:33 +0900

    2024-02-20

--- 0268. Missing Number.py
+++ 0268. Missing Number.py
@@ -1,7 +1,6 @@
+from typing import List
+
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n= len(nums)
-        n = n * (n+1) // 2
-        for i in nums:
-            n -= i
-        return n
+        return (len(nums)**2 + len(nums)) // 2 - sum(nums)

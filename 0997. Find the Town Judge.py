8f2d6b3afbf6612a6a4f5955795d5c59697ff887
ckdrkt2 <bbiuyy@gmail.com>
2024-02-22 18:14:50 +0900

    2024-02-22

--- 0997. Find the Town Judge.py
+++ 0997. Find the Town Judge.py
@@ -1,10 +1,10 @@
+from typing import List
+
+
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         count = [0] * (n + 1)
         for i, j in trust:
             count[i] -= 1
             count[j] += 1
-        for i in range(1, n + 1):
-            if count[i] == n - 1:
-                return i
-        return -1
+        return -1 if max(count) != n - 1 else count.index(max(count))

934a0f9a9df97a99409058ffa2ffc735cf3a28ab
ckdrkt2 <bbiuyy@gmail.com>
2024-03-17 23:09:32 +0900

    2024-03-17

--- 0057. Insert Interval.py
+++ 0057. Insert Interval.py
@@ -1,14 +1,16 @@
+from typing import List
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
+        ans = []
         for interval in intervals:
             if interval[1] < newInterval[0]:
-                res.append(interval)
+                ans.append(interval)
             elif interval[0] > newInterval[1]:
-                res.append(newInterval)
+                ans.append(newInterval)
                 newInterval = interval
             elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
                 newInterval[0] = min(interval[0], newInterval[0])
                 newInterval[1] = max(newInterval[1], interval[1])
-        res.append(newInterval);
-        return res
+        ans.append(newInterval);
+        return ans

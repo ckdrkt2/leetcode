101b68301441d3d578f22015189c189e33f3397a
ckdrkt2 <bbiuyy@gmail.com>
2024-03-24 21:15:16 +0900

    2024-03-24

--- 0287. Find the Duplicate Number.py
+++ 0287. Find the Duplicate Number.py
@@ -1,7 +1,16 @@
 from typing import List
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        ans = set()
-        for n in nums:
-            if n in ans: return n
-            ans.add(n)
+        slow, fast = nums[0], nums[0]
+
+        while True:
+            slow, fast = nums[slow], nums[nums[fast]]
+            if slow == fast:
+                break
+
+        fast = nums[0]
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+
+        return fast

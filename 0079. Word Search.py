163f907b0dda9e434e2e869feeb538d0e670946e
ckdrkt2 <bbiuyy@gmail.com>
2024-04-03 22:34:02 +0900

    2024-04-03

--- 0079. Word Search.py
+++ 0079. Word Search.py
@@ -1,20 +1,23 @@
+from typing import List
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n, length = len(board), len(board[0]), len(word)
-        def dfs(i, j, cnt):
-            if cnt == length - 1: return True
-            tmp = board[i][j]
-            board[i][j] = ''
-            res = False
-            for x, y in ((i+1,j),(i-1,j),(i,j+1),(i,j-1)):
-                if not (0 <= x < m and 0 <= y < n): continue
-                if board[x][y] == word[cnt+1]:
-                    res |= dfs(x, y, cnt+1)
-            board[i][j] = tmp
-            return res
-        ans = False
-        for i in range(m):
-            for j in range(n):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    ans |= dfs(i, j, 0)
-        return ans
+                    if self.dfs(i, j, 0, board, word):
+                        return True
+        return False
+
+    def dfs(self, i: int, j: int, cnt: int, board: List[List[str]], word: str) -> bool:
+        if cnt == len(word) - 1: return True
+
+        tmp, res = board[i][j], False
+        board[i][j] = ''
+        for x, y in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+            if not (0 <= x < len(board) and 0 <= y < len(board[0])):
+                continue
+            if board[x][y] == word[cnt + 1]:
+                res |= self.dfs(x, y, cnt + 1, board, word)
+        board[i][j] = tmp
+        return res

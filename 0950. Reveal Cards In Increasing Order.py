8390899905bc40392aceb18a577e0e2897d339a8
ckdrkt2 <bbiuyy@gmail.com>
2024-04-10 18:41:44 +0900

    2024-04-10

--- 0950. Reveal Cards In Increasing Order.py
+++ 0950. Reveal Cards In Increasing Order.py
@@ -1,15 +1,10 @@
-```python
+from typing import List
+
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        index = [i for i in range(len(deck))]
-        ans = [0] * len(deck)
+        index, ans = [i for i in range(len(deck))], [0] * len(deck)
         for card in sorted(deck):
             ans[index.pop(0)] = card
             if index:
                 index.append(index.pop(0))
         return ans
-```
-
-time complexity: O(NlogN)
-
-space complexity: O(N)

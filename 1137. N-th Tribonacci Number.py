b848bd82818d2fdbdc4d6e1d906f82e1b9a2da99
ckdrkt2 <bbiuyy@gmail.com>
2024-04-24 23:09:32 +0900

    2024-04-24

--- 1137. N-th Tribonacci Number.py
+++ 1137. N-th Tribonacci Number.py
@@ -1,5 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         arr = [0, 1, 1]
-        for _ in range(n-2): arr.append(sum(arr[-3:]))
+        for _ in range(n - 2):
+            arr.append(sum(arr[-3:]))
         return arr[n]

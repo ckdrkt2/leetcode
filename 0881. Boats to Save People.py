6daccf739bd73039a0c2507ed2cbe0fa16de38a6
ckdrkt2 <bbiuyy@gmail.com>
2024-05-04 18:38:53 +0900

    2024-05-04

--- 0881. Boats to Save People.py
+++ 0881. Boats to Save People.py
@@ -1,10 +1,13 @@
 from typing import List
+
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort(reverse=True)
-        ans, i, j = 0, 0, -1
-        while i - j <= len(people):
-            if people[i] + people[j] <= limit: j -= 1
-            i += 1
+        ans, l, r = 0, 0, len(people) - 1
+        while l <= r:
+            if people[l] + people[r] <= limit:
+                r -= 1
+            l += 1
             ans += 1
         return ans

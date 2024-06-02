f58a549ec6161a64cd26d09bc6fbb7c2ea649bdd
ckdrkt2 <bbiuyy@gmail.com>
2024-06-02 14:52:45 +0900

    2024-06-02

--- 0344. Reverse String.py
+++ 0344. Reverse String.py
@@ -1,3 +1,6 @@
+from typing import List
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        for i in range(len(s) // 2):
+            s[i], s[-(i + 1)] = s[-(i + 1)], s[i]

856d1e7ff85c114275a6bf5279f56cf3d9a35509
ckdrkt2 <bbiuyy@gmail.com>
2024-06-08 23:09:32 +0900

    2024-06-08

--- 0523. Continuous Subarray Sum.py
+++ 0523. Continuous Subarray Sum.py
@@ -1,16 +1,15 @@
+from typing import List
+
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if len(nums) == 1: return False
-        sums, N = [nums[0]], len(nums)
-        for num in nums[1:N]:
-            temp = num + sums[-1]
-            if temp % k == 0: return True
-            sums.append(temp)
-        subs = set()
-        rem = sums[0] % k
-        for idx in range(1, N):
-            new = sums[idx] % k
-            if new in subs: return True
-            subs.add(rem)
-            rem = new
+        hash, total = {0: 0}, 0
+        for i in range(len(nums)):
+            total += nums[i]
+
+            if total % k not in hash:
+                hash[total % k] = i + 1
+
+            elif hash[total % k] < i:
+                return True
+
         return False

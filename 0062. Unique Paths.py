aa7ae6c0f3011f5e14d88e3fb91ba8fad9e848d6
ckdrkt2 <bbiuyy@gmail.com>
2024-07-21 23:59:32 +0900

    2024-07-21

--- 0062. Unique Paths.py
+++ 0062. Unique Paths.py
@@ -1,4 +1,14 @@
-from math import factorial as f
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return f(m + n - 2) // (f(m - 1) * f(n - 1))
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[-1][-1]
